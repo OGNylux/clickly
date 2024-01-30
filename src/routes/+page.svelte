ff3955c87a9f23b19235d001babcca8278e91a96
Konrad <konradm.studium@gmail.com>
2024-01-30 21:53:37 +0100

    Add menu-screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-sussy bussy
-<a href="/game">zum spiel</a>
+<script>
+
+</script>
+
+<main class="relative h-screen bg-cover bg-no-repeat text-center" style="background-image: url('/background.jpg')">
+    <div class="bottom-0 left-0 right-0 top-0 h-full w-full" style="background-color: rgba(0, 0, 0, 0.6)">
+        <div class="flex h-full flex-col items-center justify-center">
+            <img src="emojis/uwu.svg" alt="" class="size-72 mb-10">
+            <h1 class="mb-4 text-4xl text-white font-bold">Clickly</h1>
+            <a href="/game" class="w-56 text-base relative overflow-hidden px-6 py-3 group rounded-3xl bg-slate-100 text-slate-950 mt-7">
+                <div class="absolute top-0 right-full w-full h-full bg-slate-800 opacity-20 group-hover:translate-x-full z-0 duration-500"/>
+                <h2 class="relative z-10 text-xl">Classic &#129054;</h2>
+            </a>
+            <a href="/" class="w-56 text-base relative overflow-hidden px-6 py-3 group rounded-3xl bg-slate-100 text-slate-950 mt-7">
+                <div class="absolute top-0 right-full w-full h-full bg-slate-800 opacity-20 group-hover:translate-x-full z-0 duration-500"/>
+                <h2 class="relative z-10 text-xl">Competitive &#129054;</h2>
+            </a>
+            <a href="/" class="w-56 text-base relative overflow-hidden px-6 py-3 group rounded-3xl bg-slate-100 text-slate-950 mt-7">
+                <div class="absolute top-0 right-full w-full h-full bg-slate-800 opacity-20 group-hover:translate-x-full z-0 duration-500"/>
+                <h2 class="relative z-10 text-xl">Help &#129054;</h2>
+            </a>
+        </div>
+    </div>
+</main>
