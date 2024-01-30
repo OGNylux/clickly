4375283c394b9fb3ed35e9a6eb760b2c5d583986
Konrad <konradm.studium@gmail.com>
2024-01-30 23:11:54 +0100

    Add new logo and new font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,19 +3,18 @@
 </script>
 
 <main class="relative h-screen bg-cover bg-no-repeat text-center" style="background-image: url('/background.jpg')">
-    <div class="bottom-0 left-0 right-0 top-0 h-full w-full" style="background-color: rgba(0, 0, 0, 0.6)">
+    <div class="bottom-0 left-0 right-0 top-0 h-full w-full" style="background-color: rgba(0, 0, 0, 0.7)">
         <div class="flex h-full flex-col items-center justify-center">
-            <img src="emojis/uwu.svg" alt="" class="size-72 mb-10">
-            <h1 class="mb-4 text-4xl text-white font-bold lexend">Clickly</h1>
-            <a href="/game" class="w-56 text-base relative overflow-hidden px-6 py-3 group rounded-3xl bg-slate-100 text-slate-950 mt-7">
+            <img src="emojis/logo.svg" alt="" class="size-72 mb-8">
+            <a href="/game" class="w-56 text-base relative overflow-hidden px-6 py-3 group rounded-xl bg-slate-100 text-slate-950 mt-7">
                 <div class="absolute top-0 right-full w-full h-full bg-slate-800 opacity-20 group-hover:translate-x-full z-0 duration-500"/>
                 <h2 class="relative z-10 text-xl">Classic<i class="ml-3 fa-solid fa-play fa-beat fa-lg" style="color: #0284c7;"/></h2>
             </a>
-            <a href="/" class="w-56 text-base relative overflow-hidden px-6 py-3 group rounded-3xl bg-slate-100 text-slate-950 mt-7">
+            <a href="/" class="w-56 text-base relative overflow-hidden px-6 py-3 group rounded-xl bg-slate-100 text-slate-950 mt-7">
                 <div class="absolute top-0 right-full w-full h-full bg-slate-800 opacity-20 group-hover:translate-x-full z-0 duration-500"/>
                 <h2 class="relative z-10 text-xl">Competitive <i class="ml-3 fa-solid fa-play fa-beat fa-lg" style="color: #059669;"/></h2>
             </a>
-            <a href="/" class="w-56 text-base relative overflow-hidden px-6 py-3 group rounded-3xl bg-slate-100 text-slate-950 mt-7">
+            <a href="/" class="w-56 text-base relative overflow-hidden px-6 py-3 group rounded-xl bg-slate-100 text-slate-950 mt-7">
                 <div class="absolute top-0 right-full w-full h-full bg-slate-800 opacity-20 group-hover:translate-x-full z-0 duration-500"/>
                 <h2 class="relative z-10 text-xl"><i class="mr-3 fa-solid fa-circle-info fa-flip" style="color: #b91c1c;"></i>Info</h2>
             </a>
