6e8469ba10f173af0f8831a1703fb3bc6075560b
paulpaulsen55 <paulpaulsenx@gmail.com>
2024-02-01 01:45:40 +0100

    added Building components

---
+++ src/lib/components/BuildingsWrapper.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { storeItems } from "$lib/data";
+    import Building from "./Building.svelte";
+</script>
+
+<div class="flex flex-col gap-2">
+    {#each storeItems as storeItem}
+        <Building storeItem={storeItem} />
+    {/each}
+</div>
