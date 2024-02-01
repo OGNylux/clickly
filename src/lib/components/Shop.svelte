c4caed5902ecf5fffb18be1211a753b38c37e154
Konrad <konradm.studium@gmail.com>
2024-02-01 23:23:43 +0100

    Add sell functionality to shop

--- src/lib/components/Shop.svelte
+++ src/lib/components/Shop.svelte
@@ -2,11 +2,13 @@
     import { storeItems } from "$lib/data";
     import { buildings, emojis, score } from "$lib/store";
     import { onDestroy, onMount } from "svelte";
-    import ShopItem from "./ShopItem.svelte";
+    import BuyItem from "./BuyItem.svelte";
+    import SellItem from "./SellItem.svelte";
     
     let passiveIncome = Array(8).fill(0),
         interval = 0;
     
+    let Sell = false;
 
     onMount(() => {
         interval = setInterval(() => {
@@ -24,13 +26,38 @@
     onDestroy(() => {
         clearInterval(interval);
     });
+
+    function toggleShop() {
+    Sell = !Sell;
+    }
 </script>
 
 <div class="flex flex-col gap-2">
     <p>passive income: {passiveIncome.reduce((p,a) => p+a, 0)}E/s</p>
-    {#each storeItems as storeItem}
-        <div class="flex items-center gap-2">
-            <ShopItem storeItem={storeItem} />
-        </div>
-    {/each}
+    <div class="grid grid-cols-2 bg-slate-100 rounded-xl w-24 font-medium resetBgColor">
+        <button on:click={toggleShop} class="has-[:checked]:bg-slate-300 rounded-full border-slate-200 border-2 buy_button">Buy</button>
+        <button on:click={toggleShop} class="bg-slate-300 rounded-full border-slate-200 border-2 sell_button">Sell</button>
+    </div>
+    {#if Sell}
+        {#each storeItems as storeItem}
+            <div class="flex items-center gap-2">
+                <BuyItem storeItem={storeItem} />
+            </div>
+        {/each}
+    {:else}
+        {#each storeItems as storeItem}
+            <div class="flex items-center gap-2">
+                <SellItem storeItem={storeItem} />
+            </div>
+        {/each}
+    {/if}
 </div>
+
+<style>
+    .sell_button {
+        border-radius: 0px 12px 12px 0px;
+    }
+    .buy_button {
+        border-radius: 12px 0px 0px 12px;
+    }
+</style>
