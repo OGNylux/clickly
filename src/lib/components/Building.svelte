661ac375093f3d12d8f922a1c356f449c9e26add
Sander Hahn <nylux.22@gmail.com>
2024-02-02 14:29:16 +0100

    building component

--- src/lib/components/Building.svelte
+++ src/lib/components/Building.svelte
@@ -4,9 +4,17 @@
     import Emoji from "./Emoji.svelte";
 
     export let storeItem: storeItem;
+
 </script>
 
-<div class="flex w-80 bg-slate-200 rounded-xl p-2">
-    <Emoji emoji={storeItem.component} animated={true}/>
+<div class="grid grid-cols-3 w-80 bg-slate-200 rounded-xl p-2">
+    <div class="col-span-2">
+        <p>{storeItem.name}</p>
+        <div class="flex">
+            {#each {length: Math.min($buildings[storeItem.index], 10)} as _}
+                <Emoji emoji={storeItem.component} animated={true}/>
+            {/each}
+        </div>
+    </div>
     <p class="text-5xl">{$buildings[storeItem.index]}</p>
 </div>
