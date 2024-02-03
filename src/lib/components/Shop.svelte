f2532730b28607cc99b576ff49db6d656f39bc2e
Konrad <konradm.studium@gmail.com>
2024-02-03 23:47:35 +0100

    Add functionality to show different amounts of items

--- src/lib/components/Shop.svelte
+++ src/lib/components/Shop.svelte
@@ -10,6 +10,7 @@
     
     let buy = true;
     let sell = false;
+    let numberOfItems = 1;
 
     onMount(() => {
         interval = setInterval(() => {
@@ -36,6 +37,9 @@
         buy = false;
         sell = true;
     }
+    function toggleItems(n: number) {
+        numberOfItems = n;
+    }
 </script>
 
 <div class="flex flex-col gap-2">
@@ -46,20 +50,20 @@
             <button on:click={toggleSell} class={`button2 ${sell ? 'bg-slate-100' : 'bg-slate-300'}`}>Sell</button>
         </div>
         <div class="grid grid-cols-3 w-32 font-medium border-slate-200 border-2 rounded-xl">
-            <button class="bg-slate-300 button1">1</button>
-            <button class="bg-slate-300">10</button>
-            <button class="bg-slate-300 button2">100</button>
+            <button on:click={() => toggleItems(1)} class={`button1 ${numberOfItems == 1 ? 'bg-slate-100' : 'bg-slate-300'}`}>1</button>
+            <button on:click={() => toggleItems(10)} class={`${numberOfItems == 10 ? 'bg-slate-100' : 'bg-slate-300'}`}>10</button>
+            <button on:click={() => toggleItems(100)} class={`button2 ${numberOfItems == 100 ? 'bg-slate-100' : 'bg-slate-300'}`}>100</button>
         </div>
     </div>
     {#if buy}
-        {#each storeItems as storeItem}
+        {#each storeItems.slice(0, numberOfItems) as storeItem}
             <div class="flex items-center gap-2">
                 <BuyItem storeItem={storeItem} />
             </div>
         {/each}
     {/if}
     {#if sell}
-        {#each storeItems as storeItem}
+        {#each storeItems.slice(0, numberOfItems) as storeItem}
             <div class="flex items-center gap-2">
                 <SellItem storeItem={storeItem} />
             </div>
