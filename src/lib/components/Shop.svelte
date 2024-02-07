7798f9d6ac8b144439583140ae6437823e6006b4
Konrad <konradm.studium@gmail.com>
2024-02-07 21:30:03 +0100

    Add box around shop

--- src/lib/components/Shop.svelte
+++ src/lib/components/Shop.svelte
@@ -10,8 +10,8 @@
     }
 </script>
 
-<div class="flex flex-col gap-2">
-    <div class="grid grid-flow-col grid-cols-1">
+<div class="flex flex-col gap-2 mr-1">
+    <div class="grid grid-flow-col grid-cols-1 mx-1">
         <div class="grid grid-cols-2 w-28 font-medium border-slate-200 border-2 rounded-xl">
             <button on:click={() => buy = true} class={`button1 ${buy ? 'bg-slate-100' : 'bg-slate-300'}`}>Buy</button>
             <button on:click={() => buy = false} class={`button2 ${!buy ? 'bg-slate-100' : 'bg-slate-300'}`}>Sell</button>
@@ -22,19 +22,21 @@
             <button on:click={() => toggleItems(100)} class={`button2 ${numberOfItems == 100 ? 'bg-slate-100' : 'bg-slate-300'}`}>100</button>
         </div>
     </div>
-    {#if buy}
-        {#each $unlocked as storeItem}
-            <div class="flex items-center gap-2">
-                <ShopItem storeItem={storeItem} bind:action={buy} bind:numberOfItems={numberOfItems} />
-            </div>
-        {/each}
-    {:else}
-        {#each $unlocked as storeItem}
-            <div class="flex items-center gap-2">
-                <ShopItem storeItem={storeItem} bind:action={buy} bind:numberOfItems={numberOfItems} />
-            </div>
-        {/each}
-    {/if}
+    <div class="border-slate-100 border-2 bg-slate-50 rounded-xl p-1">
+        {#if buy}
+            {#each $unlocked as storeItem}
+                <div class="flex items-center py-1">
+                    <ShopItem storeItem={storeItem} bind:action={buy} bind:numberOfItems={numberOfItems} />
+                </div>
+            {/each}
+        {:else}
+            {#each $unlocked as storeItem}
+                <div class="flex items-center py-1">
+                    <ShopItem storeItem={storeItem} bind:action={buy} bind:numberOfItems={numberOfItems} />
+                </div>
+            {/each}
+        {/if}
+    </div>
 </div>
 
 <style>
