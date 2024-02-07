e080ce255fdb73e95e7fd0d6ecd64187da2c22f4
Nylux <nylux.22@gmail.com>
2024-02-07 18:48:20 +0100

    finalized shop design

--- src/lib/components/Building.svelte
+++ src/lib/components/Building.svelte
@@ -2,14 +2,15 @@
     import type { StoreItem } from "$lib/data";
     import { buildings } from "$lib/store";
     import Emoji from "./Emoji.svelte";
+    import { ArrowBigUpDash } from 'lucide-svelte';
 
     export let storeItem: StoreItem;
 
     let test = false
 </script>
 
-<div class="grid grid-cols-3 w-80 bg-slate-200 rounded-xl">
-    <div class="col-span-2 m-2">
+<div class="flex bg-slate-200 w-96 rounded-xl">
+    <div class="m-2 w-full">
         <p>{storeItem.name}</p>
         <div class="flex h-8 -space-x-4 rtl:space-x-reverse">
             {#each {length: Math.min($buildings[storeItem.index], 10)} as _}
@@ -21,8 +22,19 @@
             {/each}
         </div>
     </div>
-    <div class="grid rounded-xl justify-center justify-items-center bg-slate-600 border-2 border-slate-200 text-white">
-        <p class="text-4xl">{$buildings[storeItem.index]}</p>
-        <p class="text-sm justify-self-center space-y-2">{$buildings[storeItem.index]*storeItem.multiplier} E/s</p>
+    <div class="flex rounded-xl bg-slate-500 justify-self-end justify-center items-center upgrade_button">
+        <button class="ml-2">
+            <ArrowBigUpDash size={40} class="text-white"/>
+        </button>
+        <div class="rounded-xl h-full w-32 text-center bg-slate-600 border-2 p-2 border-slate-200 text-white">
+            <p class="text-4xl truncate">{$buildings[storeItem.index]}</p>
+            <p class="text-sm truncate -m-2">{$buildings[storeItem.index]*storeItem.multiplier} E/s</p>
+        </div>
     </div>
 </div>
+
+<style>
+    .upgrade_button {
+        border-radius: 12px 20px 20px 12px;
+    }
+</style>
