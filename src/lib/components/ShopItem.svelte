18464e8dc86661b282ed54dd2d204c07bfb3ee9f
Calliope2 <konradm.studium@gmail.com>
2024-02-07 16:10:04 +0100

    Dopple cock

--- src/lib/components/ShopItem.svelte
+++ src/lib/components/ShopItem.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import type { StoreItem } from "$lib/data";
     import { buildings, emojis } from "$lib/store";
+    import Tooltip from "./Tooltip.svelte";
 
     export let storeItem: StoreItem;
     export let action: boolean;
@@ -42,14 +43,13 @@
 <div class="grid grid-flow-col grid-cols-4 place-content-start bg-slate-200 w-96 rounded-xl">
     <img src={storeItem.image.src} alt="" class="size-16 p-2">
     <div class="flex flex-col justify-between p-2 col-span-2">
-        <p>{storeItem.name}</p>
-        <div class="grid grid-flow-col grid-cols-2">
-            <p>${storePrice}E
-                {#if action}
-                    | {storeItem.incomeMultiplier}E/s
-                {/if}
-            </p>
-        </div>
+        <Tooltip>
+            <p>{storeItem.name}</p>
+            <p>${storePrice}E</p>
+            <div slot="tip">
+                <p>passives Einkommen:<span>{storeItem.incomeMultiplier}E/s</span></p>
+            </div>
+        </Tooltip>
     </div>
 
     <button on:click={() => action ? buy() : sell()} class="p-2 bg-slate-100 buy_button hover:bg-slate-300 transition font-bold border-slate-200 border-2">
