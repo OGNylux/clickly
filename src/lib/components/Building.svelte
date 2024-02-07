8d8beabb63e8236cb5005646fd1b148afb7dd082
Sander Hahn <nylux.22@gmail.com>
2024-02-07 17:43:58 +0100

    mütter genommen

--- src/lib/components/Building.svelte
+++ src/lib/components/Building.svelte
@@ -4,20 +4,25 @@
     import Emoji from "./Emoji.svelte";
 
     export let storeItem: StoreItem;
+
+    let test = false
 </script>
 
-<div class="grid grid-cols-3 w-80 bg-slate-200 rounded-xl p-2">
-    <div class="col-span-2">
+<div class="grid grid-cols-3 w-80 bg-slate-200 rounded-xl">
+    <div class="col-span-2 m-2">
         <p>{storeItem.name}</p>
-        <div class="flex">
+        <div class="flex h-8 -space-x-4 rtl:space-x-reverse">
             {#each {length: Math.min($buildings[storeItem.index], 10)} as _}
                 {#if storeItem.component}
-                    <Emoji emoji={storeItem.component} animated={true}/>
+                    <Emoji emoji={storeItem.component} animated={true} />
                 {:else}
-                    <img src={storeItem.image.src} alt="">
+                    <img src={storeItem.image.src} alt="" class="filter drop-shadow size-8">
                 {/if}
             {/each}
         </div>
     </div>
-    <p class="text-5xl">{$buildings[storeItem.index]}</p>
+    <div class="grid rounded-xl justify-center justify-items-center bg-slate-600 border-2 border-slate-200 text-white">
+        <p class="text-4xl">{$buildings[storeItem.index]}</p>
+        <p class="text-sm justify-self-center space-y-2">{$buildings[storeItem.index]*storeItem.multiplier} E/s</p>
+    </div>
 </div>
