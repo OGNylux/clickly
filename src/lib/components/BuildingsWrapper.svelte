857245e7d31b5a9f4f03cca03eb794c87338b358
Justus <justus.winter@urbanandmainlines.com>
2024-02-09 10:27:52 +0100

    fixed display clickerupgrade as building

--- src/lib/components/BuildingsWrapper.svelte
+++ src/lib/components/BuildingsWrapper.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-    import { unlockedPassiveItems } from "$lib/store";
+    import { unlockedClicker, unlockedPassiveItems } from "$lib/store";
     import Building from "./Building.svelte";
 </script>
 
 <div class="flex flex-col gap-2">
+    <Building storeItem={$unlockedClicker} />
     {#each $unlockedPassiveItems as item}
         <Building storeItem={item} />
     {/each}
