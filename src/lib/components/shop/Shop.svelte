f08ea0fbc2aa4c159ba75ea9b2545844c3035d42
Nylux <nylux.22@gmail.com>
2024-02-13 22:46:53 +0100

    will eig nur justus mail zuspammen

--- src/lib/components/shop/Shop.svelte
+++ src/lib/components/shop/Shop.svelte
@@ -14,28 +14,13 @@
 <div class="flex flex-col gap-2 mr-1">
     <div class="grid grid-flow-col grid-cols-1 mx-1">
         <div class="grid grid-cols-2 w-28 font-medium border-slate-200 border-2 rounded-xl">
-            <button on:click={() => (buy = true)} 
-                class={`button1 ${buy ? "bg-slate-100" : "bg-slate-300"}`}>
-                Buy
-            </button>
-            <button on:click={() => (buy = false)} 
-                class={`button2 ${!buy ? "bg-slate-100" : "bg-slate-300"}`}>
-                Sell
-            </button>
+            <button on:click={() => (buy = true)} class={`button1 ${buy ? "bg-slate-100" : "bg-slate-300"}`}>Buy</button>
+            <button on:click={() => (buy = false)} class={`button2 ${!buy ? "bg-slate-100" : "bg-slate-300"}`}>Sell</button>
         </div>
         <div class="grid grid-cols-3 w-32 font-medium border-slate-200 border-2 rounded-xl">
-            <button on:click={() => toggleItems(1)} 
-                class={`button1 ${numberOfItems == 1 ? "bg-slate-100" : "bg-slate-300"}`}>
-                1
-            </button>
-            <button on:click={() => toggleItems(10)} 
-                class={`${numberOfItems == 10 ? "bg-slate-100" : "bg-slate-300"}`}>
-                10
-            </button>
-            <button on:click={() => toggleItems(100)} 
-                class={`button2 ${numberOfItems == 100 ? "bg-slate-100" : "bg-slate-300"}`}>
-                100
-            </button>
+            <button on:click={() => toggleItems(1)} class={`button1 ${numberOfItems == 1 ? "bg-slate-100" : "bg-slate-300"}`}>1</button>
+            <button on:click={() => toggleItems(10)} class={`${numberOfItems == 10 ? "bg-slate-100" : "bg-slate-300"}`}>10</button>
+            <button on:click={() => toggleItems(100)} class={`button2 ${numberOfItems == 100 ? "bg-slate-100" : "bg-slate-300"}`}>100</button>
         </div>
     </div>
     <div class="border-slate-100 border-2 bg-slate-50 rounded-xl p-1">
