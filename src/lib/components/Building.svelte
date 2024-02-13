8daf53d6831137549688eea0e4b5b665f6ee85be
Nylux <nylux.22@gmail.com>
2024-02-13 17:46:51 +0100

    added features back

--- src/lib/components/Building.svelte
+++ src/lib/components/Building.svelte
@@ -1,15 +1,37 @@
 <script lang="ts">
-    import { StoreItem } from "$lib/data";
+    import { ClickerItem, PassiveIncomeItem, StoreItem } from "$lib/data";
+    import { ArrowBigUpDash } from "lucide-svelte";
     import Emoji from "./Emoji.svelte";
 
     export let storeItem: StoreItem;
 </script>
 
-<div class="flex w-96 bg-slate-200 rounded-xl p-2">
-    {#if storeItem.component}
-        <Emoji emoji={storeItem.component} animated={true}/>
-    {:else}
-        <img src={storeItem.image.src} alt="">
-    {/if}
-    <p class="text-5xl">{storeItem.getAmount()}</p>
+<div class="flex bg-slate-200 w-96 rounded-xl">
+    <div class="m-2 mt-1 w-full">
+        <p class="font-bold">{storeItem.name}</p>
+        <div class="flex h-8 -space-x-4 rtl:space-x-reverse">
+            {#each {length: Math.min(storeItem.getAmount(), 10)} as _}
+                {#if storeItem.component}
+                    <Emoji emoji={storeItem.component} animated={true} />
+                {:else}
+                    <img src={storeItem.image.src} alt="" class="filter drop-shadow size-8">
+                {/if}
+            {/each}
+        </div>
+    </div>
+    <div class="flex -space-x-9 rtl:space-x-reverse text-white">
+        <div class="flex rounded-xl bg-slate-500 border-slate-200 border-2">
+            <button class="ml-2 mr-10">
+                <ArrowBigUpDash size={40}/>
+            </button>
+        </div>
+        <div class="rounded-xl w-32 p-2 text-center border-2 bg-slate-600">
+            <p class="text-4xl truncate">{storeItem.getAmount()}</p>
+            {#if storeItem instanceof ClickerItem}
+                <p class="text-sm truncate -m-2">{storeItem.getInfluence()+1} E/c</p>
+            {:else if storeItem instanceof PassiveIncomeItem}
+                <p class="text-sm truncate -m-2">{storeItem.getInfluence()} E/s</p>
+            {/if}
+        </div>
+    </div>
 </div>
--- src/lib/components/BuildingsWrapper.svelte
+++ src/lib/components/BuildingsWrapper.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
     import { unlockedClicker, unlockedPassiveItems } from "$lib/store";
+    import { Separator } from "bits-ui";
     import Building from "./Building.svelte";
 </script>
 
 <div class="flex flex-col gap-2">
-    <p class="text-gray-500">Clicker</p>
     <Building storeItem={$unlockedClicker} />
-    <p class="text-gray-500">Passive Income</p>
+    <Separator.Root class="m-1 h-1 rounded bg-slate-200" />
     {#each $unlockedPassiveItems as item}
         <Building storeItem={item} />
     {/each}
