d0da26f6a504c0eab1250a5cc9ac89e031f3efd9
Konrad <87895872+Calli-ope@users.noreply.github.com>
2024-02-14 13:55:23 +0100

    Fix main (#16)
    * Fix Tooltip
    
    * fixed removeItem
    
    * Fix sell part 1000
    
    * Change color of Tooltip
    
    ---------
    
    Co-authored-by: paulpaulsen55 <paulpaulsenx@gmail.com>

--- src/lib/components/shop/ShopItem.svelte
+++ src/lib/components/shop/ShopItem.svelte
@@ -56,7 +56,7 @@
         >
             <span class="line-through">BUY</span>
         </button>
-    {:else if !item.checkRemoveAmount(numberOfItems) && !action}
+    {:else if item.getAmount() == 0 && !action}
         <div class="flex justify-center items-center">
             <Lock size={32} />
         </div>
