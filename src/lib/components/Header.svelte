33406d74bd26a5b884cf9177748f6ee16f110576
Konrad <konradm.studium@gmail.com>
2024-02-22 19:26:29 +0100

    Gibt es bei Commit-Messages eigentlich ein Zeichenlimit? Hat mich länger schon mal interessiert und ist ja auch eine spannende Frage für die Wissenschaft. Wie viel kann man hier bitte reinschreiben wtf Add BellIcon changing depending on status of read messages and reformated the header
    ok gibt anscheinend kein Zeichenlimit 🥱😦😰😱🤬🥹🤠

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import { onMount } from "svelte";
-    import { Settings, BellDot } from "lucide-svelte";
+    import { Settings, Bell } from "lucide-svelte";
     import { Popover, Separator } from "bits-ui";
     import { flyAndScale } from "$lib/transition";
     import { getLevel } from "$lib/helper";
@@ -11,66 +11,89 @@
     onMount(() => {
         level = getLevel($score);
     });
+
+    function updateReadStatus() {
+        notifications.update((notifications) => {
+            return notifications.map((notification) => {
+                return {
+                    ...notification,
+                    unread: false,
+                };
+            });
+        });
+    }
 </script>
 
-<div class="flex h-10 items-center bg-slate-200 px-3 header">
-    <div class="inline-flex h-8 items-center justify-center px-2 text-md font-medium bg-slate-300 border-slate-200 border-2 rounded-xl">
-        Username
+<div class="grid grid-cols-3 h-10 items-center bg-slate-200 px-3 header content-center">
+    <div class="place-self-start flex items-center">
+        <div class="flex h-8 px-3 bg-slate-300 border-slate-200 border-2 rounded-xl">
+            <div class="font-medium">
+                Username
+            </div>
+        </div>
+        <div class="flex items-center px-3 font-medium">
+            {`Level ${level}`}
+        </div>
     </div>
-    <div class="inline-flex h-10 items-center justify-center px-3 text-md font-medium">
-        {`Level ${level}`}
-    </div>
-    <div class="flex-1"></div>
-    <div class="inline-flex h-10 items-center justify-center px-3 text-3xl font-bold">
+    <div class="place-self-center flex items-center px-3 text-xl font-semibold">
         Clickly 🤠
     </div>
-    <div class="flex-1"></div>
-    <Popover.Root>
-        <Popover.Trigger class="inline-flex h-8 items-center justify-end px-4 transition hover:bg-slate-300 rounded-xl">
-            {#each $notifications as notification}
-                <button 
-                class={`${notification.unread ? `<img src="Bell-Icon.svg" alt="bell" class="size-8` : `<BellDot/>`}`}>
-                </button>
-            {/each}
-            <BellDot/>
-        </Popover.Trigger>
-        <Popover.Content
-            class="z-20 w-full max-w-[250px] rounded-[12px] border bg-slate-800 p-4 text-white"
-            transition={flyAndScale}
-            sideOffset={8}
-        >
-            <div class="bg-slate-800">
-                <Popover.Arrow class="rounded-[2px] border-l border-t border-slate-950" />
-            </div>
-            <div class="flex items-center justify-start">
-                <h2 class="text-lg font-bold">Notifications</h2>
-            </div>
-            <Separator.Root 
-            class="my-2 shrink-0 bg-slate-300 data-[orientation=horizontal]:h-px data-[orientation=vertical]:h-full data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-[1px]"/>
+    <div class="place-self-end flex">
+        <Popover.Root>
+            <Popover.Trigger class="items-center justify-end px-4">
+                <div class="transition hover:bg-slate-300 rounded-3xl p-1">
+                    {#if $notifications.some(notification => notification.unread)}
+                        <button on:click={() => updateReadStatus()}>
+                            <img src="Bell-Icon.svg" alt="bell" class="size-6"/>
+                        </button>
+                    {:else}
+                        <Bell />
+                    {/if}
+                </div>
+            </Popover.Trigger>
+            <Popover.Content
+                class="z-20 w-full max-w-[230px] rounded-xl border bg-slate-800 p-4 text-white"
+                transition={flyAndScale}
+                sideOffset={8}
+            >
+                <div class="bg-slate-800">
+                    <Popover.Arrow class="rounded-sm border-l border-t border-slate-950" />
+                </div>
+                <div class="flex items-center justify-center">
+                    <h2 class="text-lg font-bold">Notifications</h2>
+                </div>
+                <Separator.Root 
+                class="my-2 shrink-0 bg-slate-300 data-[orientation=horizontal]:h-px data-[orientation=vertical]:h-full data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-[1px]"/>
                 {#each $notifications as notification}
                     <div class="flex flex-col py-2">
                         {@html notification.message}
                     </div>
                 {/each}
-        </Popover.Content>
-    </Popover.Root>
-    <Popover.Root>
-        <Popover.Trigger class="inline-flex h-8 items-center justify-end px-4    transition hover:bg-slate-300 rounded-xl">
-            <Settings/>
-        </Popover.Trigger>
-        <Popover.Content
-            class="z-20 w-full max-w-[328px] rounded-[12px] border bg-slate-800 p-4 text-white"
-            transition={flyAndScale}
-            sideOffset={8}
-        >
-            <div class="bg-slate-800">
-                <Popover.Arrow class="rounded-[2px] border-l border-t border-slate-950" />
-            </div>
-            <div class="flex items-center justify-between">
-                <h2 class="text-lg font-bold">Settings</h2>
-            </div>
-        </Popover.Content>
-    </Popover.Root>
+            </Popover.Content>
+        </Popover.Root>
+        <Popover.Root>
+            <Popover.Trigger class="items-center justify-end px-4">
+                <div class="transition hover:bg-slate-300 rounded-3xl p-1">
+                    <Settings/>
+                </div>
+            </Popover.Trigger>
+            <Popover.Content
+                class="z-20 w-full max-w-[230px] rounded-xl border bg-slate-800 p-4 text-white"
+                transition={flyAndScale}
+                sideOffset={8}
+            >
+                <div class="bg-slate-800">
+                    <Popover.Arrow class="rounded-sm border-l border-t border-slate-950" />
+                </div>
+                <div class="flex items-center justify-center">
+                    <h2 class="text-lg font-bold">Settings</h2>
+                </div>
+                <Separator.Root 
+                class="my-2 shrink-0 bg-slate-300 data-[orientation=horizontal]:h-px data-[orientation=vertical]:h-full data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-[1px]"/>
+                Wer das ließt ist nen hs 😎
+            </Popover.Content>
+        </Popover.Root>
+    </div>
 </div>
 
 <style lang="postcss">
