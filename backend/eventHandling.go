0cd4dd8b16278de7dcd4486252ccfc34885b78e5
Johannes Foltin <Johannes.foltin@gmx.de>
2024-03-06 00:26:47 +0100

    structure is set lol

---
+++ backend/eventHandling.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/gorilla/websocket"
+
+// Messages to Server:
+// Update Score
+// Get Score
+// Leave Event
+
+// Messages from Server:
+// Event finished
+func handleEvent(conn *websocket.Conn, username string) {
+
+}
