0373aabb6cfe143fdc64903cae1c434beb8de7ef
Johannes Foltin <Johannes.foltin@gmx.de>
2024-03-15 21:39:32 +0100

    updated port

--- src/lib/websocket.ts
+++ src/lib/websocket.ts
@@ -3,7 +3,7 @@ export class Socket {
   private static instance: Socket;
   private ws: WebSocket;
   private constructor() {
-    this.ws = new WebSocket('wss://johafo.de/ws');
+    this.ws = new WebSocket('wss://johafo.de:14133/ws');
   }
   public static getInstance() {
     if (!Socket.instance) {
