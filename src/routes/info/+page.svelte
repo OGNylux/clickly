994263c4f9dbfba2dfed7c4d2563082c2c6e78ab
Konrad <konradm.studium@gmail.com>
2024-03-16 01:45:40 +0100

    Add Info-Page

---
+++ src/routes/info/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+
+<main class="relative h-screen bg-cover bg-no-repeat text-center" style="background-image: url('/background.jpg')">
+    <div class="bottom-0 left-0 right-0 top-0 h-full w-full" style="background-color: rgba(0, 0, 0, 0.4)">
+        <div class="flex h-full flex-col items-center justify-center text-white">
+            <h1 class="text-5xl font-bold mb-1">Erklärungen</h1>
+            <p class="text-xl mb-5">Das Video erklärt dir die Basics des Spiels Clickly, damit du direkt durchstarten kannst <br> und der nächste Emoji-Champion werden kannst!</p>
+            <iframe width="840" height="472.5" src="https://www.youtube.com/embed/yMlZ4bRXIc0?si=kkFRMUyhY_0WeAGx" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>        </div>
+    </div>
+</main>
