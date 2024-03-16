2ec84e66d157807b7cc168937a6ba13cd7e5b3b8
Nylux <nylux.22@gmail.com>
2024-03-17 00:59:18 +0100

    optimization

--- src/lib/components/events/FallingObjectManager.svelte
+++ src/lib/components/events/FallingObjectManager.svelte
@@ -6,6 +6,8 @@
     export let height = 0;
     export let score = 0;
 
+    const numOfCookies = 20;
+
     let canvas: HTMLCanvasElement | null,
         context: CanvasRenderingContext2D | null,
         fallingObjects: FallingItem[] = [],
@@ -17,8 +19,15 @@
         if (!canvas) return;
         context = canvas.getContext('2d');
 
-        for (let i = 0; i < 20; i++) {
-            fallingObjects.push(new FallingItem(fallingObjects.length, Math.random() * canvas.width, -20 - Math.random() * 100, Math.random()));
+        let cookie = new Image();
+            cookie.src = 'emojis/cookie.svg';
+            
+        for (let i = 0; i < numOfCookies; i++) {
+            let x = Math.random() * canvas.width;
+            let y = -40 - Math.random() * 100;
+            let randomize = Math.random();
+            let size = 100 + randomize * 70;
+            fallingObjects.push(new FallingItem(cookie, x, y, size, randomize));
         }
 
         requestAnimationFrame(draw);
@@ -38,31 +47,38 @@
         then = now - (elapsed % (1000 / fps));
         context.clearRect(0, 0, canvas.width, canvas.height);
 
-        for (let i = 0; i < fallingObjects.length; i++) {
-            const falling = fallingObjects[i];
-            let cookie = new Image();
-            cookie.src = 'emojis/cookie.svg';
-            let cookieOverlay = new Image();
-            cookieOverlay.src = 'emojis/cookieOverlay.svg';
-            
-            // Draw the object
-            context.drawImage(cookie, falling.x, falling.y, falling.size, falling.size);
-            context.globalAlpha = falling.opacity < 0.5 ? falling.opacity : 0.5;
-            context.drawImage(cookieOverlay, falling.x, falling.y, falling.size, falling.size);
+        fallingObjects.forEach((falling) => {
+            if (!context || !canvas) return;
+
+            if(falling.size > 140) context.drawImage(falling.img, falling.x, falling.y, falling.size, falling.size);
+
+            context.globalAlpha = 0.05;
+            context.fillStyle = "white";
+            context.fillRect(0, 0, canvas.width, canvas.height);
             context.globalAlpha = 1;
+
+            if(falling.size > 120) context.drawImage(falling.img, falling.x, falling.y, falling.size, falling.size);
+
+            context.globalAlpha = 0.05;
+            context.fillStyle = "white";
+            context.fillRect(0, 0, canvas.width, canvas.height);
+            context.globalAlpha = 1;
+
+            if (falling.size > 100) context.drawImage(falling.img, falling.x, falling.y, falling.size, falling.size);
+            
 
             falling.y += 0.025 * falling.size;
             if(falling.clicked) {
-                if(falling.size > 0.25) falling.size = falling.size * 0.8;
+                if(falling.size > 0.25) falling.size = falling.size * 0.9;
                 else {falling.y = height + 20; falling.size = 100}
             }
             if (falling.y > height + 20) {
-                falling.y = -20;
+                falling.y = -40;
                 falling.clicked = false;
                 falling.size = 100 + Math.random() * 70;
                 falling.x = Math.random() * canvas.width;
             }
-        }
+        });
     }
 
     function handleClick(event: MouseEvent) {
@@ -77,7 +93,6 @@
             if (x >= falling.x && x <= falling.x + falling.size/4 && y >= falling.y && y <= falling.y + falling.size/4) {
                 falling.clicked = true;
                 score++;
-                console.log('Score:', score);
                 break; // Exit loop after logging the click
             }
         }
