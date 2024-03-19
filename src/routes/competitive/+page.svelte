943c4b18fa842409eb5dd476fed2742bbfada77e
Nylux <nylux.22@gmail.com>
2024-03-19 22:00:39 +0100

    debug

--- src/routes/competitive/+page.svelte
+++ src/routes/competitive/+page.svelte
@@ -46,6 +46,7 @@
         socket = Socket.getInstance().getSocket();
 
         socket.onmessage = async (event) => {
+            console.log("Message from server", event.data);
             const m: ServerMessage = JSON.parse(event.data);
             if (m.type == serverMessageTypes.GameState) {
                 let gameState: GameState = JSON.parse(m.message.toString());
