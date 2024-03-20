d27878ceb9a0cbce8566dea778c34a448065a854
Johannes Foltin <Johannes.foltin@gmx.de>
2024-03-21 00:11:31 +0100

    added hardcoded size to emojiclick

--- src/lib/components/clicker/Clicker.svelte
+++ src/lib/components/clicker/Clicker.svelte
@@ -133,11 +133,13 @@
             <span class="justify-self-end">{formatNumber(nextLevelScore)}</span>
         </div>
     </div>
-    <button
-        on:click={incrementCount}
-        class="h-full transform active:scale-75 transition-transform z-10 select-none flex justify-center "
-    >
-        <img src="emojis/heart.svg" alt="" class="h-full p-2 filter drop-shadow-2xl" />
-    </button>
+    <div class="h-full w-full flex justify-center items-center">
+        <button
+            on:click={incrementCount}
+            class="transform active:scale-75 transition-transform z-10 select-none"
+        >
+            <img src="emojis/heart.svg" alt="" class="2xl:h-96 lg:h-44 xl:h-52 p-2 filter drop-shadow-2xl" />
+        </button>
+    </div>
     <ClickerCanvas bind:width />
 </div>
